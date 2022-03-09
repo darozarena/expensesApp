5eb8877dc360fff90b3678719b21bb38063dbb65
David Arozarena <darozarena@gmail.com>
2022-03-09 12:13:29 +0100

    Change name

--- .scripts/deploy-production-architecture.sh
+++ .scripts/deploy-production-architecture.sh
@@ -6,7 +6,7 @@ HEROKU_ENVIRONMENT=expenses-app-pmp
 
 echo "🏗  Building architecture..."
 echo "building dockers 🐳"
-docker build -t backend-server --build-arg NPM_TOKEN="${NPM_TOKEN}" -f Dockerfile.web .
+docker build -t backend-server --build-arg NPM_TOKEN="${NPM_TOKEN}" -f Dockerfile.app .
 
 echo "Login to Heroku Container registry"
 heroku container:login
@@ -15,6 +15,6 @@ echo "Heroku containers push 👆📦"
 heroku container:push --recursive --arg NPM_TOKEN="${NPM_TOKEN}" -a $HEROKU_ENVIRONMENT
 
 echo "Release images"
-heroku container:release web -a $HEROKU_ENVIRONMENT
+heroku container:release app -a $HEROKU_ENVIRONMENT
 
 echo "Done ✅"
