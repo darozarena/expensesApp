a1703100e909a57b20765b3e4b93f76e0d4fe34d
David Arozarena <darozarena@gmail.com>
2022-03-09 10:06:04 +0100

    Update scripts 4 and remove mongodb types

--- .scripts/deploy-production-architecture.sh
+++ .scripts/deploy-production-architecture.sh
@@ -6,8 +6,8 @@ HEROKU_ENVIRONMENT=expenses-app-production
 
 echo "🏗  Building architecture..."
 echo "building dockers 🐳"
-docker build -t zzm-backend-server --build-arg NPM_TOKEN="${NPM_TOKEN}" -f Dockerfile.web .
-docker build -t zzm-backend-release --build-arg NPM_TOKEN="${NPM_TOKEN}" -f Dockerfile.release .
+docker build -t backend-server --build-arg NPM_TOKEN="${NPM_TOKEN}" -f Dockerfile.web .
+docker build -t backend-release --build-arg NPM_TOKEN="${NPM_TOKEN}" -f Dockerfile.release .
 
 echo "Login to Heroku Container registry"
 heroku container:login
@@ -16,6 +16,6 @@ echo "Heroku containers push 👆📦"
 heroku container:push --recursive --arg NPM_TOKEN="${NPM_TOKEN}" -a $HEROKU_ENVIRONMENT
 
 echo "Release images"
-heroku container:release web worker release -a $HEROKU_ENVIRONMENT
+heroku container:release web release -a $HEROKU_ENVIRONMENT
 
 echo "Done ✅"
