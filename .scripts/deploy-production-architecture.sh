764fcbc813330bf28b4e9fcc679e211a5873ba86
David Arozarena <darozarena@gmail.com>
2022-03-09 11:32:28 +0100

    Remove mongo-migrate

--- .scripts/deploy-production-architecture.sh
+++ .scripts/deploy-production-architecture.sh
@@ -7,7 +7,6 @@ HEROKU_ENVIRONMENT=expenses-app-pmp
 echo "🏗  Building architecture..."
 echo "building dockers 🐳"
 docker build -t backend-server --build-arg NPM_TOKEN="${NPM_TOKEN}" -f Dockerfile.web .
-docker build -t backend-release --build-arg NPM_TOKEN="${NPM_TOKEN}" -f Dockerfile.release .
 
 echo "Login to Heroku Container registry"
 heroku container:login
@@ -16,6 +15,6 @@ echo "Heroku containers push 👆📦"
 heroku container:push --recursive --arg NPM_TOKEN="${NPM_TOKEN}" -a $HEROKU_ENVIRONMENT
 
 echo "Release images"
-heroku container:release web release -a $HEROKU_ENVIRONMENT
+heroku container:release web -a $HEROKU_ENVIRONMENT
 
 echo "Done ✅"
